29b94fa618b5d7655f594d45f55d05c35e13e084
Jatan Dewgun <jatan.dewgun@everoad.com>
2020-03-28 17:37:01 +0100

    ADD(SWIFT): Addition of new modules

---
+++ openclassroom-course/Swift-OOP/foundations/01-foundations.swift
@@ -0,0 +1,17 @@
+class Entertainment {
+    var name = String()
+    var cost = Float()
+}
+
+class Destination {
+    var name = String()
+    var cost = Float()
+    var entertainmentOptions = [String]()
+    var accomplishments = [String]()
+}
+
+class Adventure {
+    var amountSpent = Int()
+    var destinations = [String]()
+    var placesVisited = [String]()
+}
