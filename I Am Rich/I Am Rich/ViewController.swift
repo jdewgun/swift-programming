520dce479e24a8311b26adbff4f22f9606d4b5aa
Jatan Dewgun <jatan.dewgun@everoad.com>
2020-04-04 17:12:55 +0200

    ADD: Swift Modules

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jatan on 03/04/2020.
+//  Copyright © 2020 Jatan Dewgun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
